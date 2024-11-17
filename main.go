7d2d16018dfea04a9da31183c1c1821c686e8900
salim <lesalgeriensdesstars@gmail.com>
2024-11-17 16:55:57 +0100

    mn chatgpt ou khlass

--- main.go
+++ main.go
@@ -1,51 +1,92 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
-    "strings"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"io"
 )
 
 var err error
 const uploadDir = "./uploads"
 
 func main() {
-    err = os.Mkdir(uploadDir, os.ModePerm)
-    handErr(err)
+	
+	err = os.MkdirAll(uploadDir, os.ModePerm)
+	handleErr(err)
 
-    http.HandleFunc("POST /submit", formSubHandler)
-    port := "8080" 
-    fmt.Println("server is running in localhost:" + port)
+	
+	http.HandleFunc("POST /submit", formSubHandler)
 
-    err = http.ListenAndServe(":" + port, nil)
-    /* to also host on the lan
-    err = http.ListenAndServe("0.0.0.0:" + port, nil)
-    */
-    
+	port := "8080" 
+	fmt.Println("Server is running on localhost:" + port)
 
-    handErr(err)
+	
+	err = http.ListenAndServe(":"+port, nil)
+	handleErr(err)
 }
 
-func formSubHandler(w http.ResponseWriter, r *http.Request){
-    fmt.Println(r.Method)
+func formSubHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20) 
+	handleErr(err)
 
-    err = os.Mkdir(uploadDir+"/"+strings.ToLower(r.FormValue("teamName")), os.ModePerm)
-
-    for _, subdir := range []string{"code", "reverse"}{
-         os.Mkdir(uploadDir+"/"+strings.ToLower(r.FormValue("teamName")+"/"+subdir), os.ModePerm)
-    }
-
-    if err != nil {
-	http.Error(w, "your name is crazy", http.StatusBadRequest)
+	
+	teamName := strings.ToLower(r.FormValue("teamName"))
+	if teamName == "" {
+		http.Error(w, "Team name is required", http.StatusBadRequest)
 		return
+	}
+
+	
+	teamDir := uploadDir + "/" + teamName
+	err = os.MkdirAll(teamDir, os.ModePerm)
+	handleErr(err)
+
+	
+	for _, subdir := range []string{"code", "reverse"} {
+		err = os.MkdirAll(teamDir+"/"+subdir, os.ModePerm)
+		handleErr(err)
+	}
+
+    for _, which := range []string{"first", "second", "third"}{
+        handleFileUpload(r, w, which+"Cod", teamDir+"/code")
+        handleFileUpload(r, w, which+"Rev", teamDir+"/reverse")
     }
 
-    fmt.Fprintf(w, "thank you " + r.FormValue("teamName"))
+	
+	fmt.Fprintf(w, "Thank you, team %s! Your submission has been received.", teamName)
 }
 
-func handErr(err error){
-    if err!=nil {
-        fmt.Println(err)
-    }
+func handleFileUpload(r *http.Request, w http.ResponseWriter, fieldName, subDir string) {
+	
+	file, _, err := r.FormFile(fieldName)
+	if err != nil {
+		if err.Error() != "http: no such file" {
+			http.Error(w, "Error reading file: "+err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+	defer file.Close()
+
+	
+	dstFile, err := os.Create(subDir + "/" + fieldName)
+	if err != nil {
+		http.Error(w, "Error saving file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dstFile.Close()
+
+	
+	_, err = io.Copy(dstFile, file)
+	if err != nil {
+		http.Error(w, "Error saving file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
